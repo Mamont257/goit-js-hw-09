3d06c82191abb1f4f319614d4da52b5ec419cfcd
Volodymyr Zankevych <108954365+Mamont257@users.noreply.github.com>
2022-12-24 11:34:58 +0200

    додав зміну кольору

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const body = document.querySelector('body');
+console.dir(body);
+const id = 0;
 
+body.addEventListener('click', onClick);
+
+function onClick(evt){
+    console.dir(evt.target.hasAttribute("data-start"));
+    if(evt.target.hasAttribute("data-start")){
+        id = setInterval(()=>{body.style.backgroundColor = getRandomHexColor();},1000)
+        console.log(id);
+        // body.style.backgroundColor = getRandomHexColor();
+        // setAttribute(disabled);
+        body.children[1].setAttribute('disabled','');
+        body.children[2].removeAttribute('disabled');
+        // evt.setAttribute('disabled','');
+    } else {
+        clearInterval(id);
+    }
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+console.log(getRandomHexColor());
