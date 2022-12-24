9139a3e0099182a14e7a53637b0a273f6c075e53
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2022-12-24 22:22:46 +0000

    Deploying to gh-pages from @ Mamont257/goit-js-hw-09@d06473349942c0d13c3a27babf322009d9907ae8 🚀

--- 03-promises.60536a4b.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;n=1e3,promise=new Promise((function(o,e){var t=Math.random()>.3;setInterval((function(){t?o("Good"):e("bed")}),n)})),promise.then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}))}();
-//# sourceMappingURL=03-promises.60536a4b.js.map
--- 03-promises.9c96858c.js
+++
@@ -1,2 +0,0 @@
-var o;o=1e3,promise=new Promise(((e,n)=>{const s=Math.random()>.3;setInterval((()=>{s?e("Good"):n("bed")}),o)})),promise.then((o=>console.log(o))).catch((o=>console.log(o)));
-//# sourceMappingURL=03-promises.9c96858c.js.map
