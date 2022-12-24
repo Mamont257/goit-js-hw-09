5491f30fe3891d0ab0a3204c71b56acfa3297c19
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2022-12-24 23:55:29 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,10 @@
-function createPromise(position, delay) {
+
+
+
+
+
+
+ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
@@ -6,3 +12,22 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+// let promise;
+
+
+function createPromise(position, delay) {
+  promise = new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setInterval(() => {
+      if (shouldResolve) {
+        res('Good');
+      } else {
+        rej("bed");
+      }
+    }, delay)
+  })
+    promise.then(value => console.log(value)).catch(err => console.log(err));
+}
+
+
+createPromise(0, 1000)
